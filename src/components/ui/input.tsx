f5adac3d0fd034f39538735b7a1e3680445f3b01
Tarun Selvarasu <tarunsel12@gmail.com>
2026-02-27 06:47:41 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input text">>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -12,6 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
+        suppressHydrationWarning
         {...props}
       />
     )
